a581a4b642ccc610d77290b9e02ef3522ec1caa5
unknown <shahidmonowar.cse@gmail.com>
2022-03-19 15:34:10 +0600

    offer & services updated

--- src/Pages/Home/Offer/Offer.js
+++ src/Pages/Home/Offer/Offer.js
@@ -6,18 +6,18 @@ const Offer = () => {
         <div className='container mt-5 mb-5'>
             <h1 className="text-warning text-center banner-title py-5">Surprising Offer!</h1>
             <div className="container pt-2"></div>
-            <div className="row">
-                <div className="col-sm offer-col1">
+            <div className="row mx-2 ">
+                <div className="col-sm offer-col1 mb-2">
 
                     <div><h2>Enjoy 30% CashBack</h2> <h6>Up to $70 on all Bkash</h6></div>
 
                 </div>
-                <div className="col-sm offer-col2">
+                <div className="col-sm offer-col2 mb-2">
 
                     <div><h2>25 Extra Free On KFC</h2> <h6>Stay Home, We deliver</h6></div>
 
                 </div>
-                <div className="col-sm offer-col3">
+                <div className="col-sm offer-col3 mb-2">
                     <div><h2>Japanese Food At Home</h2> <h6>Up to $30 on all Bkash</h6></div>
 
                 </div>
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -6,13 +6,13 @@ import './Services.css';
 const Services = () => {
     return (
         <div className='container mt-5 mb-2'>
-            <div className="row">
-                <div className="col-sm service-col">
+            <div className="row mx-2">
+                <div className="col-md-6 col-lg-4 service-col">
                     <div><h3><FontAwesomeIcon icon={faCalendarCheck} /></h3></div>
                     <div><h2>Mon-Fri: 10-9PM</h2> <h6>Working Hour</h6></div>
 
                 </div>
-                <div className="col-sm service-col">
+                <div className="col-md-6 col-lg-4 service-col">
                     <div><h3><FontAwesomeIcon icon={faMapMarkedAlt} /></h3></div>
                     <div><h2>Mirpur-10 Dhaka</h2> <h6>Store Location</h6></div>
 
